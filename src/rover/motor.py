06dc3ae5a1ceb33517653a577f634e3f7667712d
James Kabbes <james.kabbes@gmail.com>
2025-02-18 08:57:40 -0600

    reverting back to live motor

--- src/rover/motor.py
+++ src/rover/motor.py
@@ -37,13 +37,13 @@ class Motor:
         if not self._POLARITY:
             speed = -speed
 
-        # with SMBus(constants.I2C_BUS) as bus:
+        with SMBus(constants.I2C_BUS) as bus:
 
-        #     def f() -> None:
-        #         msg = i2c_msg.write(
-        #             self._I2C_ADDR, [register, speed.to_bytes(1, 'little', signed=True)[0]])
-        #         bus.i2c_rdwr(msg)
-        #     try:
-        #         f()
-        #     except:
-        #         f()
+            def f() -> None:
+                msg = i2c_msg.write(
+                    self._I2C_ADDR, [register, speed.to_bytes(1, 'little', signed=True)[0]])
+                bus.i2c_rdwr(msg)
+            try:
+                f()
+            except:
+                f()
