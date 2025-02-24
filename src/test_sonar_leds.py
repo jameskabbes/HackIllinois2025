ad3482e1727e547c64b383875bd59c017ab1fc46
James Kabbes <james.kabbes@gmail.com>
2025-02-24 15:16:13 -0600

    adding more test

--- src/test_sonar_leds.py
+++ src/test_sonar_leds.py
@@ -1,6 +1,31 @@
 from rover.sonar_led import SonarLEDS
 import time
+import signal
+
 
 sonar_leds = SonarLEDS()
-while True:
+
+if __name__ == '__main__':
+
+    def signal_handler(sig, frame):
+        sonar_leds.left.setPixelColor(0)
+        sonar_leds.right.setPixelColor(0)
+        exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    sonar_leds.left.setPixelColor(0xFF0000)
+    sonar_leds.right.setPixelColor(0x00FF00)
+    time.sleep(1)
+    sonar_leds.left.setPixelColor(0x00FF00)
+    sonar_leds.right.setPixelColor(0xFF0000)
+    time.sleep(1)
+    sonar_leds.left.setPixelColor(0x0000FF)
+    sonar_leds.right.setPixelColor(0xFF0000)
+    time.sleep(1)
+    sonar_leds.left.setPixelColor(0xFF0000)
+    sonar_leds.right.setPixelColor(0x0000FF)
+    time.sleep(1)
+
     sonar_leds.startSymphony()
+    time.sleep(10)
